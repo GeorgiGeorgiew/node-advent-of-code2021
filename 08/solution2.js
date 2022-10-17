48b0520aeaf7f17de760368b2c99716b93229feb
Georgi Georgiew <georgi3@gmx.net>
2022-10-17 19:39:58 +0200

    solved second part of day 08

--- 08/solution2.js
+++ 08/solution2.js
@@ -7,7 +7,7 @@ fs.readFile('./input', 'utf8' , (err, data) => {
       return
     }
 
-     const DIGIT_LEDS = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
+    //const DIGIT_LEDS = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
 
     //  aaaa
     // b    c
@@ -17,27 +17,30 @@ fs.readFile('./input', 'utf8' , (err, data) => {
     // e    f
     //  gggg
 
+    // Solved by some deductive steps
 
     lines = data.split("\n").filter(l => l.trim().length>0)
-    ledsForDigit = new Array(10)
     sum = 0
     for(line of lines) {
         lineSeparated = line.split("|")
         signalPattern = lineSeparated[0].trim().split(" ")
         outputValues = lineSeparated[1].trim().split(" ")
 
+
+        ledsForDigit = new Array(10)
+    
         // I know the numbers 1,4,7,8 through their number of digits (cf, bcdf ,acf, abcdefg)
-        ledsForDigit[1] = signalPattern.filter(ptrn => ptrn.length==2)[0]
-        ledsForDigit[7] = signalPattern.filter(ptrn => ptrn.length==3)[0]
-        ledsForDigit[4] = signalPattern.filter(ptrn => ptrn.length==4)[0]
-        ledsForDigit[8] = signalPattern.filter(ptrn => ptrn.length==7)[0]
+        ledsForDigit[1] = signalPattern.find(ptrn => ptrn.length==2)
+        ledsForDigit[7] = signalPattern.find(ptrn => ptrn.length==3)
+        ledsForDigit[4] = signalPattern.find(ptrn => ptrn.length==4)
+        ledsForDigit[8] = signalPattern.find(ptrn => ptrn.length==7)
         
         //  6 -> 6 digits and cf not both in this (now also we know which is c, which is f)
         ledsForDigit[6] = signalPattern.filter(ptrn => ptrn.length==6)
-        .filter(ptrn => ptrn.indexOf(ledsForDigit[1].charAt(0))==-1 || ptrn.indexOf(ledsForDigit[1].charAt(1)==-1))[0]
+        .find(ptrn => ptrn.indexOf(ledsForDigit[1].charAt(0))==-1 || ptrn.indexOf(ledsForDigit[1].charAt(1))==-1)
 
         // now also we know which is c, which is f
-        correctFLed = ledsForDigit[1].split('').filter(led => ledsForDigit[6].split('').indexOf(led) != -1)[0]
+        correctFLed = ledsForDigit[1].split('').find(led => ledsForDigit[6].split('').indexOf(led) != -1)
         correctCLed = ledsForDigit[1].indexOf(correctFLed) == 0 ? ledsForDigit[1].charAt(1) : ledsForDigit[1].charAt(0)   
 
         //2 -> 5 digits and only c of (cf)
@@ -54,26 +57,28 @@ fs.readFile('./input', 'utf8' , (err, data) => {
         .filter(ptrn => ptrn.indexOf(correctFLed)!=-1)[0]
         
         // the one led in 6 and not in 5 is the correct E led
-        correctELed = ledsForDigit[6].split('').filter(led => ledsForDigit[5].indexOf(led)==-1)[0] 
+        correctELed = ledsForDigit[6].split('').find(led => ledsForDigit[5].indexOf(led)==-1) 
 
         // 0 is the one remaining 6 led pattern where original E led is present
         ledsForDigit[0] = signalPattern.filter(ptrn => ptrn.length==6)
-        .filter(ptrn => ptrn != ledsForDigit[6]).filter(ptrn => ptrn.indexOf(correctELed)!=-1)[0]
+        .filter(ptrn => ptrn != ledsForDigit[6]).find(ptrn => ptrn.indexOf(correctELed)!=-1)
         // 9 is remaining 6 led pattern
         ledsForDigit[9] = signalPattern.filter(ptrn => ptrn.length==6)
-        .filter(ptrn => ptrn != ledsForDigit[6]).filter(ptrn => ptrn != ledsForDigit[0])[0]
+        .filter(ptrn => ptrn != ledsForDigit[6]).find(ptrn => ptrn != ledsForDigit[0])
 
-        // DECODE output values
-        for(outputValue of outputValues) {
-            for(candDecoded = 0; candDecoded <= 9; candDecoded++) {
-                if(ledsForDigit[candDecoded].length == outputValue.length
-                    && ledsForDigit[candDecoded].split('').filter(led => outputValue.charAt(led)!=-1).length == ledsForDigit.length) {
-                        sum += candDecoded
-                        break
-                    }
-            }
-        }
+
+        decoded = outputValues.map(oval => decodePtrn(oval, ledsForDigit)).map(a => a.toString())
+        decodedNumForLine = parseInt(decoded.join(''))
+        sum += decodedNumForLine
     }
     console.log(sum)
 
 })
+
+function isLedDigitPermutation(ptrn1, ptrn2) {
+    return ptrn1.length == ptrn2.length && ptrn1.split('').find(elem => ptrn2.indexOf(elem)==-1) == undefined
+}
+
+function decodePtrn(ptrn, ledsForDigit) {
+    return ledsForDigit.findIndex(led => isLedDigitPermutation(ptrn, led))
+}
